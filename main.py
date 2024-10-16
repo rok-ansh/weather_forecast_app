f7b42082238087f6b7106036e58928b0d73040ac
rohit <rohit26av@gmail.com>
2024-10-16 09:43:43 +0530

    Take the place, forecast_day, temperature or Sky data from API

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly_express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 
@@ -10,15 +11,7 @@ option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-
-def get_data(days):
-    dates = ["2024-20-10", "2024-21-10", "2024-22-10"]
-    temperature = [12, 15, 20]
-    temperature = [days * i for i in temperature]
-    return dates, temperature
-
-
-dates, temperature = get_data(days)
+dates, temperature = get_data(place, days, option)
 
 figure = px.line(x=dates, y=temperature,
                  labels={'x': 'Date', 'y': 'Temperature(C)'})
